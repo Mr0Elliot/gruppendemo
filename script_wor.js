547e7775d11b9f71a2a1ee083f9b50137e08f489
Mr0Elliot <106836156+Mr0Elliot@users.noreply.github.com>
2025-01-24 11:31:29 +0100

    Update script_wor.js

--- script_wor.js
+++ script_wor.js
@@ -1,4 +1,4 @@
 function infoWOR() {
-  meinung = document.getElementById("m_wor")
-  meinung.innerHTML = "WOR ist der beste";
+  meinung = document.getElementById("m_eldeniz")
+  meinung.innerHTML = "eldeniz ist der beste";
 }
